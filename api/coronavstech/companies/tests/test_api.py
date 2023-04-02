5677b8500c465f845c46bbc58a915f1e13f981f1
dan <dchestnyh21@gmail.com>
2023-04-02 16:36:03 +0300

    Refactored test into pytest format

--- api/coronavstech/companies/tests/test_api.py
+++ api/coronavstech/companies/tests/test_api.py
@@ -7,6 +7,9 @@ from rest_framework import status
 from companies.models import Company
 
 
+company_list_url = reverse("companies-list")
+
+
 @pytest.mark.django_db
 class BasicCompanyAPITestCase(TestCase):
     """Base class for all the TestCases."""
@@ -16,76 +19,85 @@ class BasicCompanyAPITestCase(TestCase):
         self.company_list_url = reverse("companies-list")
 
 
-class TestGetCompanies(BasicCompanyAPITestCase):
-    def test_zero_companies_should_return_empty_list(self) -> None:
-        """Test companies list view returns empty json when no companies are added."""
-        res = self.client.get(self.company_list_url)
+@pytest.mark.django_db
+def test_zero_companies_should_return_empty_list(client) -> None:
+    """Test companies list view returns empty json when no companies are added."""
+    res = client.get(company_list_url)
 
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(len(res.data), 0)
-
-    def test_one_company_exists_should_succeed(self) -> None:
-        """Test returning one company if it exists in db."""
-        amazon = Company.objects.create(name="Amazon")
-        res = self.client.get(self.company_list_url)
-
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(res.data[0]["name"], amazon.name)
-        self.assertEqual(res.data[0]["company_status"], amazon.company_status)
-        self.assertEqual(res.data[0]["note"], amazon.note)
-        self.assertEqual(res.data[0]["link"], amazon.link)
-
-        amazon.delete()
+    assert res.status_code == 200
+    assert res.data == []
 
 
-class TestPostCompaniesBasicAPIs(BasicCompanyAPITestCase):
-    def test_create_company_with_empty_payload_should_fail(self) -> None:
-        """Test creating a company with no name will raise an error."""
-        res = self.client.post(self.company_list_url)
+@pytest.mark.django_db
+def test_one_company_exists_should_succeed(client) -> None:
+    """Test returning one company if it exists in db."""
+    amazon = Company.objects.create(name="Amazon")
+    res = client.get(company_list_url)
 
-        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(res.data, {"name": ["This field is required."]})
+    assert res.status_code == 200
+    assert res.data[0]["name"] == amazon.name
+    assert res.data[0]["company_status"] == amazon.company_status
+    assert res.data[0]["note"] == amazon.note
+    assert res.data[0]["link"] == amazon.link
 
-    def test_create_existing_company_should_fail(self) -> None:
-        """Test creating a company which already exists will raise an error."""
-        amazon = Company.objects.create(name="Amazon")
-        payload = {"name": "Amazon"}
+    amazon.delete()
 
-        res = self.client.post(self.company_list_url, payload)
 
-        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(res.data, {"name": ["company with this name already exists."]})
+@pytest.mark.django_db
+def test_create_company_with_empty_payload_should_fail(client) -> None:
+    """Test creating a company with no name will raise an error."""
+    res = client.post(company_list_url)
 
-    def test_create_company_with_only_name_success(self) -> None:
-        """Test creating a company providing only its name succeed."""
-        payload = {"name": "Amazon"}
-        res = self.client.post(self.company_list_url, payload)
+    assert res.status_code == status.HTTP_400_BAD_REQUEST
+    assert res.data == {"name": ["This field is required."]}
 
-        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(res.data["name"], "Amazon")
-        self.assertEqual(res.data["company_status"], "Hiring")
-        self.assertEqual(res.data["link"], "")
-        self.assertEqual(res.data["note"], "")
 
-    def test_create_company_with_layoffs_status_should_succeed(self) -> None:
-        """Test creating company with layoffs status should succeed."""
-        payload = {"name": "Amazon", "company_status": "Layoffs"}
-        res = self.client.post(self.company_list_url, payload)
+@pytest.mark.django_db
+def test_create_existing_company_should_fail(client) -> None:
+    """Test creating a company which already exists will raise an error."""
+    Company.objects.create(name="Amazon")
+    payload = {"name": "Amazon"}
+    res = client.post(company_list_url, payload)
 
-        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(res.data["name"], "Amazon")
-        self.assertEqual(res.data["company_status"], "Layoffs")
-        self.assertEqual(res.data["link"], "")
-        self.assertEqual(res.data["note"], "")
+    assert res.status_code == status.HTTP_400_BAD_REQUEST
+    assert res.data == {"name": ["company with this name already exists."]}
 
-    def test_create_company_with_wrong_status_should_fail(self) -> None:
-        """Test creating company with the wrong status will raise an error."""
-        payload = {"name": "Amazon", "company_status": "Layoff"}
-        res = self.client.post(self.company_list_url, payload)
 
-        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn("is not a valid choice.", str(res.data))
-        self.assertIn("Layoff", str(res.data))
+@pytest.mark.django_db
+def test_create_company_with_only_name_success(client) -> None:
+    """Test creating a company providing only its name succeed."""
+    payload = {"name": "Amazon"}
+    res = client.post(company_list_url, payload)
+
+    assert res.status_code == status.HTTP_201_CREATED
+    assert res.data["name"] == "Amazon"
+    assert res.data["company_status"] == "Hiring"
+    assert res.data["link"] == ""
+    assert res.data["note"] == ""
+
+
+@pytest.mark.django_db
+def test_create_company_with_layoffs_status_should_succeed(client) -> None:
+    """Test creating company with layoffs status should succeed."""
+    payload = {"name": "Amazon", "company_status": "Layoffs"}
+    res = client.post(company_list_url, payload)
+
+    assert res.status_code == status.HTTP_201_CREATED
+    assert res.data["name"] == "Amazon"
+    assert res.data["company_status"] == "Layoffs"
+    assert res.data["link"] == ""
+    assert res.data["note"] == ""
+
+
+@pytest.mark.django_db
+def test_create_company_with_wrong_status_should_fail(client) -> None:
+    """Test creating company with the wrong status will raise an error."""
+    payload = {"name": "Amazon", "company_status": "Layoff"}
+    res = client.post(company_list_url, payload)
+
+    assert res.status_code == status.HTTP_400_BAD_REQUEST
+    assert "is not a valid choice." in str(res.data)
+    assert "Layoff" in str(res.data)
 
 
 def raise_covid19_exception() -> None:
@@ -99,7 +111,13 @@ def test_raise_covid_exception_should_succeed() -> None:
     assert "Coronavirus Exception" == str(e.value)
 
 
+@pytest.mark.xfail
+def test_xfail() -> None:
+    assert 1 == 2
+
+
 import logging
+
 
 logger = logging.getLogger("CORONA_LOGS")
 
