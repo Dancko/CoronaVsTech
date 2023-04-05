cdc2fb17f005e1deafbbe6e8b7ff22a97cc1624e
dan <dchestnyh21@gmail.com>
2023-04-05 21:17:25 +0300

    Added timetracker fixture to the tests

--- fibonacci/tests/test_fibonacci.py
+++ fibonacci/tests/test_fibonacci.py
@@ -2,6 +2,7 @@ from typing import Callable
 
 from fibonacci.naive import fibonacci
 from fibonacci.cached import cached_fibs, lru_cached_fibs
+from fixtures import time_tracker
 import pytest
 
 
@@ -11,9 +12,9 @@ import pytest
 #
 #     assert res == expected
 
+@pytest.mark.parametrize("n, expected", [(40, 102334155)])
 @pytest.mark.parametrize("fib_func", [fibonacci, cached_fibs, lru_cached_fibs])
-@pytest.mark.parametrize("n, expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
-def test_fibs_functions(fib_func: Callable[[int], int], n: int, expected: int) -> None:
+def test_fibs_functions(time_tracker, fib_func: Callable[[int], int], n: int, expected: int) -> None:
     res = fib_func(n)
 
     assert res == expected
